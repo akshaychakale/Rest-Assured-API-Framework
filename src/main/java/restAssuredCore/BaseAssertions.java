b600c9d696dca94142f6a38b61e9f2ff1b4b6702
akshaychakale <akshaychakale@gmail.com>
2025-03-19 23:37:30 +0530

    initial commit

---
+++ src/main/java/restAssuredCore/BaseAssertions.java
@@ -0,0 +1,11 @@
+package restAssuredCore;
+
+import io.restassured.response.Response;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+
+public class BaseAssertions {
+
+
+}
